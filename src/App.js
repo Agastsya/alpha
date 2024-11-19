8e0fc7ec38b79763749ab467aff679b370b2fec4
Agastsya Joshi <agastsya007@gmail.com>
2024-11-20 00:42:38 +0530

    added header and fixed footer

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import Register from "./components/Register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <Router>
       <div>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
         </Routes>
-        <Footer />
       </div>
+      <Footer />
     </Router>
   );
 }
