d6d162ae6fadb0ff4e001c56baae27fd6be4a727
NITIN1907 <nitindogra1907@gmail.com>
2024-11-20 00:53:21 +0530

    update new

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </Router>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,32 @@
 import React from "react";
-
+import loginImg from "../images/loginForm.svg";
+import './Login.css';
 function Login() {
   return (
     <>
-      <div>Sign up</div>
+      <div className="hero-sections">
+        <div className="photo">
+    <img src={loginImg} alt="Login"/>
+        </div>
+        <div className="section">
+          <h1 className="register-heading">
+             <span className="registerSpanner">Login</span> Yourself
+          </h1>
+          <form >
+            <label>Email</label>
+            <br />
+            <input type="email" placeholder="Email" id="email" className="inputbox" />
+            <br />
+            <label>Password</label>
+            <br />
+            <input type="password" placeholder="Password" id="password" className="inputbox"/>
+            <br />
+            <button type="submit" className="button">Register </button>
+          </form>
+          
+        </div>
+      </div>
+
     </>
   );
 }
